1998e330b0d29ff68882c93be7fc63653e87319d
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2021-12-13 19:15:56 +0000

    All input fields validated

--- app/forms.py
+++ app/forms.py
@@ -8,26 +8,32 @@ from wtforms import PasswordField
 from wtforms import BooleanField
 from wtforms import ValidationError
 
-from wtforms.validators import DataRequired,EqualTo, NumberRange, Length
+from wtforms.validators import InputRequired,EqualTo, NumberRange, Length, Required
 from app import models
 from flask import flash, session
 from flask_login import current_user
 
 # form to add modules
 class ModuleForm(Form):
-    title = TextField('title', validators=[DataRequired()])
-    module_code = TextField('module_code', validators=[DataRequired()])
-    num_of_assessments = IntegerField('num_of_assessments', validators=[DataRequired()])
-    num_of_credits = IntegerField('num_of_credits', validators=[DataRequired()])
+    title = TextField('title', validators=[InputRequired()])
+    module_code = TextField('module_code', validators=[InputRequired()])
+    num_of_assessments = IntegerField('num_of_assessments', validators=[InputRequired(), NumberRange(min=0 , max=100,
+                                                            message='Must be between 0 and 1000000') ])
+    num_of_credits = IntegerField('num_of_credits', validators=[InputRequired()])
+
+    #validating score is not greater than total marks
+    def validate_assessments(self, field):
+        flash('Validating num of assessments')
+        # flash(type(self.num_of_assessments.data))
 
 # form to add assessements
 class AssessmentForm(Form):
-    title = TextField('title', validators=[DataRequired()])
-    score = IntegerField('score', validators=[DataRequired()])
-    total_marks = IntegerField('total_marks', validators=[DataRequired()])
+    title = TextField('title', validators=[InputRequired()])
+    score = IntegerField('score', validators=[InputRequired()])
+    total_marks = IntegerField('total_marks', validators=[InputRequired()])
     assessment_worth = IntegerField('assessment_worth',
                                     validators=[
-                                                DataRequired(), 
+                                                InputRequired(), 
                                                 NumberRange(min=0 , max=100,
                                                             message='Must be between 0 and 100')])
 
@@ -37,17 +43,19 @@ class AssessmentForm(Form):
         assessment = models.Assessments.query.filter_by(student_id=current_user.id).filter_by(module_code=module_code).filter_by(title=title.data).first()
         if assessment:
             raise ValidationError('Try Again! You have already added an assessment with this title for this module!')
-
+    #validating score is not greater than total marks
     def validate_score(self, score):
         if self.score.data > self.total_marks.data:
             raise ValidationError('Your score is greater than total marks')
 
+    
+
 
 class SignUpForm(Form):
-    name = TextField('name', validators=[DataRequired()])
-    username = TextField('username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('password1', validators=[DataRequired()])
-    password2 = PasswordField('password2', validators=[EqualTo('password'), DataRequired()]) #makes sure password1 equals password2
+    name = TextField('name', validators=[InputRequired()])
+    username = TextField('username', validators=[InputRequired(), Length(min=2, max=20)])
+    password = PasswordField('password1', validators=[InputRequired()])
+    password2 = PasswordField('password2', validators=[EqualTo('password'), InputRequired()]) #makes sure password1 equals password2
 
     #validating that the username is unique
     def validate_username(self, username):
@@ -57,14 +65,14 @@ class SignUpForm(Form):
             raise ValidationError('Username taken! Please choose a different username')
 
 class LoginForm(Form):
-    username = TextField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    username = TextField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
     remember = BooleanField('Remember Me')
 
 class ButtonForm(Form):
     submit = SubmitField('Submit')
 
 class PasswordForm(Form):
-    old_password = PasswordField('old_password', validators=[DataRequired()])
-    new_password = PasswordField('new_password', validators=[DataRequired()])
-    confirm_password = PasswordField('confirm_password', validators=[EqualTo('new_password', "Password fields do not match"), DataRequired()])
+    old_password = PasswordField('old_password', validators=[InputRequired()])
+    new_password = PasswordField('new_password', validators=[InputRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[EqualTo('new_password', "Password fields do not match"), InputRequired()])
